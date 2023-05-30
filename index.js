4edab2733d0b5e4409f45ba5b152b40002ac551e
faaiz <email>
2023-05-30 10:51:36 +0500

    fiole system done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const {sub,add}=require("./module")
 const file=require('fs');
-// file.mkdirSync("filefolder")
-file.writeFileSync('./filefolder/test.txt',"Hello world")
 
 // console.log(add(1,5))
+// file.mkdirSync("filefolder")
+// file.writeFileSync('./filefolder/test.txt',"Hello world")
+
+
 
