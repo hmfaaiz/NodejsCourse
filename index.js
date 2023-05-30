71b3e800aa3cab43115f8970f868f9b272133715
faaiz <email>
2023-05-30 15:41:29 +0500

    created web Server

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const {sub,add}=require("./module")
-const file=require('fs');
+const { sub, add } = require("./module")
+const file = require('fs');
 
 // console.log(add(1,5))
+
 // file.mkdirSync("filefolder")
-// file.writeFileSync('./filefolder/test.txt',"Hello world")
+// file.writeFileSync('./filefolder/test.txt',`${new Date().toLocaleString()} Hello world\n`)
+file.appendFileSync('./filefolder/test.txt',`${new Date().toLocaleString()} This is again append statement\n`)
+const read = file.readFileSync('./filefolder/test.txt', 'utf-8')
+const stats = file.statSync('./filefolder/test.txt')
+const readableDate = {
+    "Creation": new Date(stats.birthtimeMs).toLocaleString(),
+    "Access": new Date(stats.atimeMs).toLocaleString(),
+    "Modified": new Date(stats.mtimeMs).toLocaleString(),
+    "LastAccess": new Date(stats.ctimeMs).toLocaleString()
+}
+
+// file.cpSync("./filefolder/test.txt","copy.text")
+// file.unlinkSync("copy.text")
+console.log(read)
+// console.log(stats)
+console.log(readableDate)
+
+
+
 
 
 
